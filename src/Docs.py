d07440bc993711eb51470bc07546c8dd132c60ea
Dylan Andres Cabezas Ramirez <166071032+DylanCabezas@users.noreply.github.com>
2025-07-13 00:07:54 -0500

    Create Docs.py

---
+++ src/Docs.py
@@ -0,0 +1,21 @@
+import os
+
+def handler(event, context):
+    try:
+        ruta = os.path.join(os.path.dirname(__file__), 'openapi.yaml')
+        with open(ruta, 'r') as f:
+            contenido = f.read()
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'text/yaml'
+            },
+            'body': contenido
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error al cargar la documentación: {str(e)}'
+        }
