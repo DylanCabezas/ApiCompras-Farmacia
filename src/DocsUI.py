1563348c194aec0b3fa1f369086681170ce33213
Dylan Andres Cabezas Ramirez <166071032+DylanCabezas@users.noreply.github.com>
2025-07-14 23:53:33 -0500

    Update DocsUI.py

--- src/DocsUI.py
+++ src/DocsUI.py
@@ -1,37 +1,41 @@
-html = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Swagger UI</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
-</head>
-<body>
-  <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-  <script>
-    window.onload = () => {
-      SwaggerUIBundle({
-        url: "https://f2h3buxe11.execute-api.us-east-1.amazonaws.com/dev/docs",
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIBundle.SwaggerUIStandalonePreset
-        ],
-        layout: "BaseLayout"
-      });
-    };
-  </script>
-</body>
-</html>
-'''
-
 def handler(event, context):
+    host = event['headers'].get('Host', 'localhost')
+    stage = event['requestContext']['stage']
+
+    html = f'''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Swagger UI</title>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
+      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+      <script>
+        window.onload = () => {{
+          SwaggerUIBundle({{
+            url: "https://{host}/{stage}/docs",
+            dom_id: '#swagger-ui',
+            deepLinking: true,
+            presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIBundle.SwaggerUIStandalonePreset
+            ],
+            layout: "BaseLayout"
+          }});
+        }};
+      </script>
+    </body>
+    </html>
+    '''
+
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html',
+            'Access-Control-Allow-Origin': '*'
         },
         'body': html
     }
